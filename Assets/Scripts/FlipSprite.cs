cd94d9785166d8d874e01d0ba8f17f705a90aaf3
Friiis <lukasfriis2003@hotmail.com>
2022-10-07 18:17:23 +0200

    GAMING

--- Assets/Scripts/FlipSprite.cs
+++ Assets/Scripts/FlipSprite.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class FlipSprite : MonoBehaviour
 {
     public Vector2 PointerPosition { get; set; }
 
+    PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     private void Update()
     {
-        Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
-        
+        if (view.IsMine)
+        {
+            Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
 
-        Vector2 scale = transform.localScale;
-        if (direction.x < 0)
-        {
-            scale.x = -1;
+            Vector2 scale = transform.localScale;
+            if (direction.x < 0)
+            {
+                scale.x = -1;
+            }
+            else if (direction.x > 0)
+            {
+                scale.x = 1;
+            }
+            transform.localScale = scale;
         }
-        else if (direction.x > 0)
-        {
-            scale.x = 1;
-        }
-        transform.localScale = scale;
     }
 }
--- Assets/Scripts/WeaponParent.cs
+++ Assets/Scripts/WeaponParent.cs
@@ -1,25 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class WeaponParent : MonoBehaviour
 {
     public Vector2 PointerPosition { get; set; }
 
+    PhotonView view;
+
+    private void Start()
+    {
+        view = GetComponent<PhotonView>();
+    }
+
     private void Update()
     {
-        Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
-        transform.right = direction;
+        if (view.IsMine)
+        {
+            Vector2 direction = (PointerPosition - (Vector2)transform.position).normalized;
+            transform.right = direction;
 
-        Vector2 scale = transform.localScale;
-        if (direction.x < 0)
-        {
-            scale.y = -1;
+            Vector2 scale = transform.localScale;
+            if (direction.x < 0)
+            {
+                scale.y = -1;
+            }
+            else if (direction.x > 0)
+            {
+                scale.y = 1;
+            }
+            transform.localScale = scale;
         }
-        else if (direction.x > 0)
-        {
-            scale.y = 1;
-        }
-        transform.localScale = scale;
     }
 }
