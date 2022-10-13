3005b4312794967b97bc7a5f51a73dd981736864
SilviAs5 <74591941+SilviAs5@users.noreply.github.com>
2022-10-13 11:31:52 +0200

    button

---
+++ Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class ButtonSound : MonoBehaviour
+{
+    AudioSource audioData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        audioData = GetComponent<blipSelect1>();
+        audioData = GetComponent<blipSelect2>();
+        audioData.Play(0);
+        
+    }
+
+    // Update is called once per frame
+    void OnGUI()
+    {
+        if()
+    }
+}
