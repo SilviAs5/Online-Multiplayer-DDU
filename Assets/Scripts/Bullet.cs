3adabc077645ca8db611174b12e1cca8d9f087df
Friiis <lukasfriis2003@hotmail.com>
2022-11-01 13:21:50 +0100

    okji

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,17 +7,15 @@ public class Bullet : MonoBehaviour
 {
     private BoxCollider2D bc;
     private Rigidbody2D rb;
-    private Vector2 pos;
+    private Transform pos;
+    [SerializeField] private float bulletForce = 20f;
+
 
     private void Start()
     {
         bc = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
-        pos = GetComponent<Transform>().position;
-    }
-
-    private void Update()
-    {
-        
+        pos = GetComponent<Transform>();
+        rb.AddForce(pos.right * bulletForce, ForceMode2D.Impulse);
     }
 }
